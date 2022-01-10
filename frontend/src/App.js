84035b257bc5b44656ce44f552565cbdd3d7633d
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-10 14:57:04 +0530

    refactor ; add Route from react-router-dom.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screen/HomeScreen';
 function App() {
