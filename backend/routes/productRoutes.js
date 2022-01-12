df999f22cb7884147d0be9e2cf52d005ee1a196c
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-12 21:15:17 +0530

    refactor : move route handler to controller.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,9 @@
+import {getProducts,getProductById} from './../controllers/productController.js'
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
 const router=express.Router()
 
+router.route('/').get(getProducts)
 
-//@desc     Fetch all products
-//@route    GET /api/products
-//@access   Public
-
-router.get('/',asyncHandler(async (req,res)=>{
-    const products=await Product.find({})
-    res.json(products)
-}))
-
-//@desc     Fetch a single product
-//@route    GET /api/products/:id
-//@access   Public
-router.get('/:id',asyncHandler(async(req,res)=>{
-    const product=await Product.findById(req.params.id)
-    if(product){
-        return res.json(product)
-    }else{
-        res.status(404)
-        throw new Error("Product not found")
-    }
-}))
+router.route('/:id').get(getProductById)
 
 export default router
