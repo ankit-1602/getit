4db17d1cad1e8e7f583deba0fb04804745ddecae
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-12 15:12:17 +0530

    refactor : loader and message.

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,8 +1,10 @@
 import React,{useEffect} from 'react';
-import {useDispatch,useSelector} from 'react-redux'
+import {useDispatch,useSelector} from 'react-redux';
 import {Row,Col} from 'react-bootstrap';
 import Product from './../components/Product';
-import { listProducts } from './../actions/productActions'
+import Message from './../components/Message';
+import Loader from './../components/Loader';
+import { listProducts } from './../actions/productActions';
 const HomeScreen = () => {
     const dispatch = useDispatch()
     
@@ -17,7 +19,7 @@ const HomeScreen = () => {
         <div>
             <h1>Latest Products</h1>
             {
-                loading ? ( <h1>Loading...</h1> ) : error ? ( <h1>{error}</h1> ) :
+                loading ? ( <Loader /> ) : error ? ( <Message variant='danger'>{error}</Message> ) :
             (<Row>
                 {products.map((product)=>(
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
