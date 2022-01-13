e7e2c98111c619c63a3c6f981e1c6bd9764e7d6f
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-13 09:57:01 +0530

    Add login using redux.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,28 @@
 import {combineReducers,createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {productListReducer,productDetailsReducer} from './reducers/productReducers'
-import {cartReducer} from './reducers/cartReducer'
+import {productListReducer,productDetailsReducer} from './reducers/productReducers.js'
+import {cartReducer} from './reducers/cartReducer';
+import { userLoginReducer } from './reducers/userReducer'
+
 const reducer=combineReducers({
     productList: productListReducer,
     productDetails:productDetailsReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    userLogin:userLoginReducer
 })
 
-const cartItemFromStorage=localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [] 
+const cartItemFromStorage=localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [] ;
+const userInfoFromStorage=localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 
 const initialState={
     cart: {
         cartItems:cartItemFromStorage
+    },
+    userLogin:{
+        userInfo:userInfoFromStorage
     }
+
 }
 const middleware=[thunk]
 
