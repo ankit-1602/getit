ba2a5c668b8625df4253e9e87e17511cb12f5138
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-13 11:40:39 +0530

    Add Login Screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,16 @@ import { Container } from 'react-bootstrap';
 import HomeScreen from './screen/HomeScreen';
 import ProductScreen from "./screen/ProductScreen";
 import CartScreen from "./screen/CartScreen";
+import LoginScreen from "./screen/LoginScreen";
 function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/product/:id" component={ProductScreen} />
-          <Route exact path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
           <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -34,7 +34,7 @@ export const login = (email,password) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type:USER_LOGIN_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            payload: (error.response && error.response.data.message) ? (error.response.data.message) : (error.message)
         })
     }
 }
