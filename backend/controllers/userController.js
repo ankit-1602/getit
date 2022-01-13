f70c2ee87943166973a92f8e4df599bc7a7f723a
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-13 08:48:12 +0530

    Add authentication.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from './../models/userModel.js';
+import generateToken from './../utils/generateToken.js';
 
 //@desc     Auth User & Get Token
 //@route    POST /api/users/login
@@ -13,10 +14,64 @@ export const authUser = asyncHandler(async (req,res)=>{
             _id:user._id,
             name:user.name,
             isAdmin: user.isAdmin,
-            token:null
+            token:generateToken(user._id)
         })
     }else{
         res.status(401)
         throw new Error('Invalid Email or Password.')
     }
+})
+
+
+//@desc     Get User Profile
+//@route    GET /api/users/profile
+//@access   Private
+export const getUserProfile = asyncHandler(async (req,res)=>{
+    const user=await User.findById(req.user._id)
+
+    if(user){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin: user.isAdmin
+        })
+    }else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+//@desc     Register a new user
+//@route    POST /api/users
+//@access   Public
+
+export const registerUser=asyncHandler(async (req,res)=>{
+    const {name,email,password}=req.body
+    const userExists=await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user=await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin: user.isAdmin,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
 })
