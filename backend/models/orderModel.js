ea7f26667785e306ac96a9e4601a173fef5514f0
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-14 22:26:48 +0530

    refactor : remove state from model.

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -22,7 +22,6 @@ const orderSchema=Schema({
     ],
     shippingAddress:{
         address: {type: String,required: true},
-        state: {type: String,required: true},
         city: {type: String,required: true},
         postalCode: {type: String,required: true},
         country: {type: String,required: true}
