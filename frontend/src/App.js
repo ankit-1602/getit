783de96b6921b7bd769fc2ac2dc59d8c303faa5c
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-14 17:26:03 +0530

    Add Shipping Screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,14 @@ import CartScreen from "./screen/CartScreen";
 import LoginScreen from "./screen/LoginScreen";
 import RegisterScreen from "./screen/RegisterScreen";
 import ProfileScreen from "./screen/ProfileScreen";
+import ShippingScreen from "./screen/ShippingScreen";
 function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
         <Container>
+          <Route path="/shipping" component={ShippingScreen}/>
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
