932b80b485cd40f6e6bea15b1ff876ea4c39056b
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-15 16:15:33 +0530

    feat : delete user by admin

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,8 @@ import {
     userRegisterReducer,
     userDetailsReducer,
     userUpdateProfileReducer,
-    userListReducer
+    userListReducer,
+    userDeleteReducer
 } from './reducers/userReducer'
 
 const reducer=combineReducers({
@@ -26,6 +27,7 @@ const reducer=combineReducers({
     userDetails:userDetailsReducer,
     userUpdateProfile:userUpdateProfileReducer,
     userList: userListReducer,
+    userDelete:userDeleteReducer,
     orderCreate:orderCreateReducer,
     orderDetails:orderDetailsReducer,
     orderPay:orderPayReducer,
