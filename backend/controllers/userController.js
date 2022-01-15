fa460ab933fe0a19e0a3b4f92954fbf5ad2aa3f3
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-15 15:03:23 +0530

    add getAllUsers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,3 +102,11 @@ export const registerUser=asyncHandler(async (req,res)=>{
     }
     
 })
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+export const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+})
