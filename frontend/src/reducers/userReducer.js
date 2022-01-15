7ad8dd00603c81368247e96acceb0e6ed0e9d6a4
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-15 15:10:33 +0530

    Add redux to get all users.

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -13,7 +13,11 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_RESET
+    USER_UPDATE_PROFILE_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_RESET,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from './../constants/userConstants';
 
 export const userLoginReducer=(state={ },action)=>{
@@ -90,4 +94,17 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     }
   }
 
-  
+  export const userListReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+      case USER_LIST_REQUEST:
+        return { loading: true }
+      case USER_LIST_SUCCESS:
+        return { loading: false, users: action.payload }
+      case USER_LIST_FAIL:
+        return { loading: false, error: action.payload }
+      case USER_LIST_RESET:
+        return { users: [] }
+      default:
+        return state
+    }
+  }
