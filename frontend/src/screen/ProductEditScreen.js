52974ff0e98b17b263e6d012524d8e482ab5338e
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-16 12:43:02 +0530

    refactor : image upload.

--- frontend/src/screen/ProductEditScreen.js
+++ frontend/src/screen/ProductEditScreen.js
@@ -1,6 +1,7 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { Form,Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -13,6 +14,7 @@ const ProductEditScreen = ({ match, history }) => {
   const [name, setName] = useState('')
   const [price, setPrice] = useState(0)
   const [image, setImage] = useState('')
+  const [uploadImg,setUploadImg]= useState('')
   const [brand, setBrand] = useState('')
   const [category, setCategory] = useState('')
   const [countInStock, setCountInStock] = useState(0)
@@ -43,6 +45,7 @@ const ProductEditScreen = ({ match, history }) => {
         setName(product.name)
         setPrice(product.price)
         setImage(product.image)
+
         setBrand(product.brand)
         setCategory(product.category)
         setCountInStock(product.countInStock)
@@ -51,22 +54,28 @@ const ProductEditScreen = ({ match, history }) => {
     }
   }, [dispatch, history, productId, product, successUpdate])
 
-const uploadFileHandler=(e)=>{
-  e.preventDefault()
-  dispatch(
-    updateProduct({
-      _id: productId,
-      name,
-      price,
-      image,
-      brand,
-      category,
-      description,
-      countInStock,
-    })
-  )
-}
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('image', file)
+    setUploading(true)
 
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+
+      const { data } = await axios.post('/api/upload', formData, config)
+
+      setImage(data)
+      setUploading(false)
+    } catch (error) {
+      console.error(error)
+      setUploading(false)
+    }
+  }
 const submitHandler = (e) => {
   e.preventDefault()
   dispatch(
@@ -116,6 +125,19 @@ const submitHandler = (e) => {
                 onChange={(e) => setPrice(e.target.value)}
               ></Form.Control>
             </Form.Group>
+
+            <Form.Group controlId='image'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter image url'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+              
+              {uploading && <Loader />}
+            </Form.Group>
+
 
             <Form.Group controlId='brand'>
               <Form.Label>Brand</Form.Label>
