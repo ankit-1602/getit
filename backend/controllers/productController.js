a7c3c10e08a6a5d976937359f1306e0495f33aa9
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-16 16:48:55 +0530

    refactor : add regex.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,22 @@
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
-//@desc     Fetch all products
-//@route    GET /api/products
-//@access   Public
-export const getProducts=asyncHandler(async (req,res)=>{
-    const products=await Product.find({})
-    res.json(products)
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+export const getProducts = asyncHandler(async (req, res) => {
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
+
+  const products = await Product.find({ ...keyword })
+
+  res.json(products)
 })
 
 //@desc     Fetch a single product
