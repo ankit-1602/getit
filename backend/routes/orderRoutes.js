2af03726d3d753881d9e3f381b393f60d4cef5f0
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-16 14:10:31 +0530

    refactor :  controller to update delivery status

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,7 +5,8 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
-  getOrders
+  getOrders,
+  updateOrderToDelivered
 } from '../controllers/orderController.js'
 import { protect,admin } from './../middleware/authMiddleware.js'
 
@@ -22,6 +23,9 @@ router
   .route('/:id')
   .get(protect,getOrderById)
   
+router
+  .route('/:id/deliver')
+  .put(protect,admin,updateOrderToDelivered)
 router
   .route('/:id/pay')
   .put(protect, updateOrderToPaid)
