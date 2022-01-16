e5839a48a66c633d30f86df8d7da521a036a9bfe
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-16 12:42:01 +0530

    Add Upload image controller using multer.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'
 import {errorHandler,notFound} from './middleware/errorMiddleware.js';
 dotenv.config();
 connectDB();
@@ -25,6 +26,7 @@ app.get('/',(req,res)=>{
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes)
+app.use('/api/upload',uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
