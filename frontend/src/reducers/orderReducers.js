848605606dac5e59aed6b804e24c9a409d9b4ae0
Ankit Roy <roy.ankit1602@gmail.com>
2022-01-16 14:05:15 +0530

    add redux to get all orders for admin.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -13,7 +13,10 @@ import {
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
     ORDER_LIST_MY_RESET,
-    ORDER_LIST_MY_FAIL
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_FAIL,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_REQUEST,
 } from './../constants/orderConstants.js';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -110,3 +113,25 @@ export const orderCreateReducer = (state = {}, action) => {
         return state
     }
   }
+
+  export const orderListReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+      case ORDER_LIST_REQUEST:
+        return {
+          loading: true,
+        }
+      case ORDER_LIST_SUCCESS:
+        return {
+          loading: false,
+          orders: action.payload,
+        }
+      case ORDER_LIST_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+      default:
+        return state
+    }
+  }
+  
